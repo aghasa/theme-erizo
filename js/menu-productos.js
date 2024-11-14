d618b484fe9242586259cc9ccbe54bd3ccd9ef2c
Celia Rubio Pais <rubiopaiscelia@gmail.com>
2024-11-14 17:35:10 +0100

    responsive

--- js/menu-productos.js
+++ js/menu-productos.js
@@ -1,72 +1,76 @@
-document.addEventListener("DOMContentLoaded", function () {
+// document.addEventListener("DOMContentLoaded", function () {
 
-    //recorre los elementos del menu->familias y subfamilias
-    familyItems.forEach((item, index) => {
-        const subfamilyList = item.querySelector(".daterium-subfamilia-list");
-        const toggleButton = item.querySelector(".daterium-toggle-button");
-        const familyId = `#mf${index}`;
-        const familyContainer = document.querySelector(familyId);
+//     //recorre los elementos del menu->familias y subfamilias
+//     familyItems.forEach((item, index) => {
+//         const subfamilyList = item.querySelector(".daterium-subfamilia-list");
+//         const toggleButton = item.querySelector(".daterium-toggle-button");
+//         const familyId = `#mf${index}`;
 
         
-        // Flecha -> subfamilias del menú
-        toggleButton.addEventListener("click", function (e) {
-            e.stopPropagation();
-            subfamilyList.classList.toggle("active");
-            toggleButton.innerHTML = subfamilyList.classList.contains("active") ? "-" : "+";
+//         // Flecha -> subfamilias del menú
+//         toggleButton.addEventListener("click", function (e) {
+//             e.stopPropagation();
+//             subfamilyList.classList.toggle("active");
+//             toggleButton.innerHTML = subfamilyList.classList.contains("active") ? "-" : "+";
 
-            //ocultar subfamilis si hay otras abiertas
-            document.querySelectorAll(".daterium-subfamilia-list.active").forEach(list => {
-                if (list !== subfamilyList) {
-                    list.classList.remove("active");
-                }
-            });
+//             //ocultar subfamilis si hay otras abiertas
+//             document.querySelectorAll(".daterium-subfamilia-list.active").forEach(list => {
+//                 if (list !== subfamilyList) {
+//                     list.classList.remove("active");
+//                 }
+//             });
 
-            //resetea la flecha cuando se pulsa otra
-            document.querySelectorAll(".daterium-toggle-button").forEach(button => {
-                if (button !== toggleButton) {
-                    button.innerHTML = "+"; 
-                }
-            });
-        });
+//             //resetea la flecha cuando se pulsa otra
+//             document.querySelectorAll(".daterium-toggle-button").forEach(button => {
+//                 if (button !== toggleButton) {
+//                     button.innerHTML = "+"; 
+//                 }
+//             });
+//         });
 
-        // Subfamilia -> mostrar solo esos productos
-        const subfamilyItems = subfamilyList.querySelectorAll(".daterium-brand-menu-subitem");
-        subfamilyItems.forEach((subItem, subIndex) => {
-            const subfamilyId = `#mf${index}sf${subIndex}`;
-            const subfamilyContainer = document.querySelector(subfamilyId);
-
-            subItem.addEventListener("click", function (e) {
-                e.stopPropagation();
-
-                document.querySelectorAll(".daterium-brand-family").forEach(fam => {
-                    fam.style.display = "none";
-                });
-
-                // solo se ven productos de esa subcategoria
-                familyContainer.querySelectorAll(".daterium-brand-subfamily").forEach(sub => {
-                    sub.style.display = "none";
-                });
-
-                familyContainer.style.display = "block";
-                subfamilyContainer.style.display = "block";
-            });
-        });
-    });
-});
+//         // Subfamilia -> mostrar solo esos productos
+//         const subfamilyItems = subfamilyList.querySelectorAll(".daterium-brand-menu-subitem");
+        
+//     });
+// });
 
 
-const familyItems = document.querySelectorAll(".daterium-brand-menu-list-item");
 function mostrarFamilia(familyContainer) {
-    document.querySelectorAll(".daterium-brand-family").forEach(fam => {
-        fam.style.display = "none";
+  if(familyContainer != familyActive){
+    document.querySelectorAll(".daterium-brand-family").forEach((fam) => {
+    fam.style.display = "none";
+  });
+  familyContainer.style.display = "block";
+  familyContainer
+    .querySelectorAll(".daterium-brand-subfamily")
+    .forEach((sub) => {
+      sub.style.display = "block";
     });
-    familyContainer.style.display = "block";
-    familyContainer.querySelectorAll(".daterium-brand-subfamily").forEach(sub => {
-        sub.style.display = "block";
+
+     menu_familia_nombre = '#menu-familia-' + toString(id_familia);
+
+    familyActive = id_familia;  
+    
+} else {
+    document.querySelectorAll(".daterium-brand-family").forEach((fam)  => {
+        fam.style.display = "block";
+      });
+    familyActive = 0;
+    }
+}
+/*
+function mostrarProdSubfamilia(familyContainer, subfamilyContainer) {
+  //ocultar familias
+  document.querySelectorAll(".daterium-brand-family").forEach((fam) => {
+    fam.style.display = "none";
+  });
+  //ocultar las demas subfamilias
+  familyContainer
+    .querySelectorAll(".daterium-brand-subfamily")
+    .forEach((sub) => {
+      sub.style.display = "none";
     });
-}
 
-const button = document.querySelectorAll(".daterium-toggle-button");
-function mostrarSubfamilias(){
-
-}
+  familyContainer.style.display = "block";
+  subfamilyContainer.style.display = "block";
+}*/
