48298acd17210d63647a18f18d8fe18f2b7b18e8
Celia Rubio Pais <rubiopaiscelia@gmail.com>
2024-11-15 10:40:19 +0100

    mostrar subfamilias

--- js/menu-productos.js
+++ js/menu-productos.js
@@ -1,30 +1,45 @@
-
-
 function mostrarFamilia(familyContainer) {
-  if(familyContainer != familyActive){
+  if (familyContainer != familyActive) {
+
+    familyContainerDiv =  document.getElementById(familyContainer);
     document.querySelectorAll(".daterium-brand-family").forEach((fam) => {
-    fam.style.display = "none";
-  });
-  familyContainer.style.display = "block";
-  familyContainer
-    .querySelectorAll(".daterium-brand-subfamily")
-    .forEach((sub) => {
-      sub.style.display = "block";
+      fam.style.display = "none";
     });
-
-     menu_familia_nombre = '#menu-familia-' + toString(familyContainer);
-
-    familyActive = familyContainer;  
-    
-} else {
-    document.querySelectorAll(".daterium-brand-family").forEach((fam)  => {
-        fam.style.display = "block";
+    familyContainerDiv.style.display = "block";
+    familyContainerDiv
+      .querySelectorAll(".daterium-brand-subfamily")
+      .forEach((sub) => {
+        sub.style.display = "block";
       });
+    familyActive = familyContainer;
+  } else {
+    document.querySelectorAll(".daterium-brand-family").forEach((fam) => {
+      fam.style.display = "block";
+    });
     familyActive = 0;
-    }
+  }
 }
 
-function mostrarSubfamilias(){
+function mostrarSubfamilias(button, familiaId) {
+  console.log(familiaId);
+  var subfamiliaShow = document.getElementById(familiaId);
+
+  var subfamiliasList = document.querySelectorAll(".daterium-subfamilia-list");
+
+  subfamiliasList.forEach((x) => {
+    x.style.display = "none";
+  });
   
+  subfamiliaShow.style.display = "flex";
+/*
+  if (
+    subfamiliasList.style.display === "none" ||
+    subfamiliasList.style.display === ""
+  ) {
+    subfamiliasList.style.display = "block";
+    button.textContent = "-";
+  } else {
+    subfamiliasList.style.display = "none";
+    button.textContent = "+";
+  }*/
 }
-
